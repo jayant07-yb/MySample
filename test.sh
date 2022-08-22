825dfaa826c2db13ccd5ce6b8ba13952b9929f2f
Jayant Anand <janand@yugabyte.com>
2022-08-22 17:10:19 +0530

    Added the odyssey's endpoint

--- test.sh
+++ test.sh
@@ -2,7 +2,7 @@
 mvn -q package exec:java -DskipTests -Dexec.mainClass=com.yugabyte.PerfTest -Dexec.args="help"
 
 PgBouncerConnectionURL="jdbc:postgresql://10.150.4.254:5400/yugabyte"
-OdysseyConnectionURL="jdbc:postgresql://10.150.4.254:5400/yugabyte"
+OdysseyConnectionURL="jdbc:postgresql://10.150.1.213:5400/yugabyte"
 
 NumberOfThreads="1 5 10 50 100 300 500 700 1000"
 Tests="WRITE READ"
@@ -19,6 +19,7 @@ do
   for numberThreads in $NumberOfThreads
     do
       mvn -q package exec:java -DskipTests -Dexec.mainClass=com.yugabyte.PerfTest -Dexec.args="$PgBouncerConnectionURL $UserName $Password $testname $numberThreads $CommitFrequency $LoopSize"
+  echo "---------------------------------------------------"
   done
 done
 
@@ -31,6 +32,7 @@ do
   for numberThreads in $NumberOfThreads
     do
       mvn -q package exec:java -DskipTests -Dexec.mainClass=com.yugabyte.PerfTest -Dexec.args="$OdysseyConnectionURL $username $password $testname $numberThreads $CommitFrequency $LoopSize"
+   echo "---------------------------------------------------"
   done
 done
 
